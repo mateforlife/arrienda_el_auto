9d6dd028e20b82d99834c4ef5281422f54c716e9
nachovai <reyes.s.ign@gmail.com>
2019-11-24 17:41:31 -0300

    user_form changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :second_last_name, :rut, :phone_number, :birthdate, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :second_last_name, :rut, :phone_number, :birthdate, :gender])
   end
 end
