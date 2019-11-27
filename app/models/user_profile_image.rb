8e58b2f4b3c300ee81ee99c098c57c177c7bb390
mateforlife <rodriguez.ruiz.alex@gmail.com>
2019-11-27 18:25:20 -0300

    remove useless models

--- app/models/user_profile_image.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# UserProfileImage
-class UserProfileImage < ProfileImage
-end
--- app/models/vehicle_image.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# VehicleImage
-class VehicleImage < ProfileImage
-  
-end
