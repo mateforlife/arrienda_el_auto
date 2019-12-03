b193139c71435b3b4cab07e20a97a20abfbd2ebb
nachovai <reyes.s.ign@gmail.com>
2019-12-02 21:09:49 -0300

    wip

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @vehicles = Vehicle.search(params[:search]).available
-                       .not_from_current_user(current_user)
-                       .preload(:profile_images)
+    if params[:search].blank?
+      redirect_to root_path, notice: 'Empty field'
+    else
+      @vehicles = Vehicle.search(params[:search]).available
+                         .not_from_current_user(current_user)
+                         .preload(:profile_images)
+    end
   end
 end
