ada70459c6081176d729cacb6eecdd1f2b6316cb
mateforlife <rodriguez.ruiz.alex@gmail.com>
2019-12-04 22:49:37 -0300

    hotfix: remove confirmable user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :vehicles
   validates :rut, presence: true
 
