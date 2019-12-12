181c5331f8610ddc82410048e616a4fd28fad080
mateforlife <rodriguez.ruiz.alex@gmail.com>
2019-12-11 22:48:19 -0300

    remove flash types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
-  add_flash_types :info, :error, :warning
-
   protected
 
   def configure_permitted_parameters
