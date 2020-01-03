166f95359343591dec151d3792af642611387d60
mateforlife <rodriguez.ruiz.alex@gmail.com>
2020-01-03 00:56:33 -0300

    hotfix: remove include missing helper

--- app/controllers/validate_legal_documents_controller.rb
+++ app/controllers/validate_legal_documents_controller.rb
@@ -1,6 +1,7 @@
-class ValidateLegalDocumentsController < ApplicationController
-  include ValidateLegalDocumentsHelper
+# frozen_string_literal: true
 
+# ValidateLegalDocumentsController
+class ValidateLegalDocumentsController < ApplicationController
   def index
     @vehicles = Vehicle.pending_document_validation
     @users = User.pending_document_validation
