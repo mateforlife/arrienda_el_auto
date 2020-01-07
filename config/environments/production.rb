92079ad3202c619948fc2afbec0a9658bbb07be2
mateforlife <rodriguez.ruiz.alex@gmail.com>
2020-01-07 14:37:13 -0300

    add https to default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http://easy-car.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://easy-car.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
 
