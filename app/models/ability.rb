d32fdc305874104e27526a88040a02d818f0e203
mateforlife <rodriguez.ruiz.alex@gmail.com>
2020-01-22 20:27:28 -0300

    fix: reservation ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,18 +11,17 @@ class Ability
     elsif user.basic?
       can :manage, Vehicle, user_id: user.id
       can :read, Vehicle, status: %i[published rented]
-      # legal_documents_from_user_resources = LegalDocument.resources_from_user(user).pluck(:id)
       can :read, LegalDocument
       can :create, LegalDocument
       can :manage, DriverAccount, user_id: user.id
       can :manage, User, id: user.id
-      cannot :index, Reservation
       if user&.driver_account&.legal_documents_effective?
-        can %i[read edit create update destroy], Reservation, user_id: user.id
+        can %i[index read edit create update destroy], Reservation, user_id: user.id
+        can :new, Reservation
       end
       can %i[read new create], Payment, reservation: { user: { id: user.id } }
     end
     vehicles_id = user.vehicles.pluck(:id)
-    cannot :create, Reservation, vehicle_id: vehicles_id
+    cannot %i[new create], Reservation, vehicle_id: vehicles_id
   end
 end
