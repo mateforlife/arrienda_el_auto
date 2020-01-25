c82aacfda33df476142197ddead8705bf2c8f79c
mateforlife <rodriguez.ruiz.alex@gmail.com>
2020-01-24 21:20:01 -0300

    remove: useless code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,8 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   before_action :set_locale
   before_action :authenticate_user!
-  before_action :user_status
 
   private
-
-  def user_status
-    return unless current_user
-
-
-  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
